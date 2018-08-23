186fec80461172924e75532ccbc6643438cb3499
Supun Dileepa <dilee1st.1995@gmail.com>
2018-08-23 10:28:04 +0530

    chnage

--- Internship/resources/views/welcome.blade.php
+++ Internship/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Learn About Laravel
                 </div>
 
                 <div class="links">
