6fb107a3528c204942ba68979a834b1f4e210d55
okazakikirito <okazakikirito@gmail.com>
2018-09-04 21:40:13 +0530

    SPM2018
    new link added

--- spm/storage/framework/views/fda9496893e42ba2da2bf7beb720169fa848a0ff.php
+++ spm/storage/framework/views/fda9496893e42ba2da2bf7beb720169fa848a0ff.php
@@ -5,27 +5,27 @@
       <ul class="navbar-nav mr-auto">
 
         <li class="nav-item active">
-          <a class="nav-link" href="/spm/public/home">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="SPM2018//spm/public/home">Home <span class="sr-only">(current)</span></a>
         </li>
 
         <li class="nav-item active">
-          <a class="nav-link" href="/spm/public/form/i1">Form I-1 <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="SPM2018//spm/public/form/i1">Form I-1 <span class="sr-only">(current)</span></a>
         </li> 
 
          <li class="nav-item active">
-          <a class="nav-link" href="/spm/public/form/i3">Form I-3 <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="SPM2018//spm/public/form/i3">Form I-3 <span class="sr-only">(current)</span></a>
         </li> 
 
          <li class="nav-item active">
-          <a class="nav-link" href="/spm/public/form/i5">Form I-5 <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="SPM2018//spm/public/form/i5">Form I-5 <span class="sr-only">(current)</span></a>
         </li>
 
           <li class="nav-item active">
-          <a class="nav-link" href="/spm/public/form/i6">Form I-6 <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="SPM2018//spm/public/form/i6">Form I-6 <span class="sr-only">(current)</span></a>
         </li>
 
           <li class="nav-item active">
-          <a class="nav-link" href="/spm/public/form/i7">Form I-7 <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="SPM2018//spm/public/form/i7">Form I-7 <span class="sr-only">(current)</span></a>
         </li>
 
       </ul>
