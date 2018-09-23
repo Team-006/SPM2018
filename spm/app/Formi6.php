88c41e0466ce7dd6183b38146f59db98413f899f
Supun Dileepa <dilee1st.1995@gmail.com>
2018-09-24 00:57:48 +0530

    model created

--- spm/app/Formi6.php
+++ spm/app/Formi6.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Formi6 extends Model
 {
-    //
+    protected $fillable = ['company','project','ojectives','procedures','methodolgy','learning','measurable_outcome','effectiveness'];
 }
